9c532f2caf5bcc4b050ec7dfef8ddb2923f4d256
DevarshiKSagar <devarshi.sagar@gmail.com>
2020-12-07 13:20:48 +0530

    last commit

--- 101203070_comp3123_a2/src/App.js
+++ 101203070_comp3123_a2/src/App.js
@@ -5,6 +5,11 @@ import Forecast from "./components/Forecast/forecast";
 function App() {
   return (
     <div className="App">
+      <link
+        href="https://fonts.googleapis.com/css?family=Montserrat:400,900"
+        rel="stylesheet"
+      />
+
       <header className="App-header">
         <h1>Check Weather </h1>
       </header>
--- 101203070_comp3123_a2/src/components/Forecast/forecast.js
+++ 101203070_comp3123_a2/src/components/Forecast/forecast.js
@@ -1,45 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { useState } from "react";
-
+import "../../App.css";
 const Forecast = () => {
-  let [data, setData] = useState({});
   let [city, setCity] = useState("");
-  // const uriEncodedCity = encodeURIComponent(city);
-  function getForecast(e) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=6e00f7d59ac5b3e25afe0901361f3d45`
-    )
-      .then((response) => {
-        console.log(
-          "api call :"`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=6e00f7d59ac5b3e25afe0901361f3d45`
-        );
-        console.log("in the first then before setdata is called");
+  let [data, setData] = useState({});
 
-        return response.json();
+  function drawWeather() {
+    var celcius = Math.round(parseFloat(data.main.temp));
+
+    var description = data.weather[0].description;
+    var iconcode = data.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/wn/" + iconcode + ".png";
+    document.getElementById("wicon").src = iconurl;
+    document.getElementById("wicon").style = "background: white;";
+    document.getElementById("description").innerHTML = description;
+    document.getElementById("temp").innerHTML = celcius + "&deg;";
+    document.getElementById("location").innerHTML = data.name;
+
+    if (description.indexOf("rain") > 0) {
+      document.main.className = "rainy";
+    } else if (description.indexOf("cloud") > 0) {
+      document.main.className = "cloudy";
+    } else if (description.indexOf("sunny") > 0) {
+      document.main.className = "sunny";
+    }
+  }
+
+  function getForecast(e) {
+    e.preventDefault();
+
+    setData({});
+
+    const uriEncodedCity = encodeURIComponent(city);
+    console.log("line 32");
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${uriEncodedCity}&appid=6e00f7d59ac5b3e25afe0901361f3d45`
+      )
+      .then((response) => {
+        return response.data;
       })
       .then((response) => {
-        console.log("in the second then before setdata is called" + response);
-        setData(JSON.stringify(response));
+        if (response.cod !== 200) {
+          throw new Error();
+        }
+        console.log();
+        setData(response);
         console.log(data);
+        drawWeather();
       })
       .catch((err) => {
-        console.error(err);
+        console.log(err.message);
       });
   }
   return (
     <div>
-      <h2>Get Current Weather Conditions</h2>
+      <div id="description"></div>
+      <div id="temp"></div>
+      <div id="icon">
+        <img id="wicon" alt="wheather icon" />
+      </div>
+
+      <div id="location"></div>
+      <h2>Find Current Weather Conditions</h2>
       <form onSubmit={getForecast}>
         <input
           type="text"
-          placeholder="Enter the City name to get wheather data"
-          maxLength="70"
+          placeholder="Enter the name of the City"
+          maxLength="50"
           value={city}
-          onChange={(t) => setCity(t.target.value)}
+          onChange={(e) => setCity(e.target.value)}
         />
 
-        <button type="submit">Find out wheather</button>
+        <button type="submit">Get Forecast</button>
       </form>
     </div>
   );
